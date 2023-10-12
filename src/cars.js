7c312b44da4f2ac2e447cdd126a528932e97ea06
jonathans199 <jso1919@gmail.com>
2023-10-12 16:37:26 -0400

    updated code base

---
+++ src/cars.js
@@ -0,0 +1,18 @@
+// CRUD - cars collection 
+// C - CREATE
+// R - READ 
+// U - UPDATE
+// D - DELETE
+
+// 1. Create cars.js - right next to fish.js
+// 2. Create a function to add/create a car
+function createCar(){}
+
+// 3. Create a function to read/get all cars
+function readAllCars(){}
+
+// 4. Create a function to delete one car
+function deleteACar(){}
+
+// 5. Create function to Update on car
+function updateCar(){}
