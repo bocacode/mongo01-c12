743ced8452b7aa310c8331bfb484dff64a62cdf0
Todd H. Albert <todd@bocacode.com>
2023-10-13 10:24:16 -0400

    added catches

--- src/fish.js
+++ src/fish.js
@@ -3,17 +3,19 @@ import { db } from './connectDb.js'
 
 const fishDb = db.collection('fish')
 
+// DRY == Don't Repeat Yourself
+
 const theFish = {
-	species: 'tetro',
-	origin: 'Amazon',
-	color: 'silver',
-	size: '1"',
-	family: {
-		dad: '🐠',
-		sister: '🍣',
-		mom: { cool: true, special: undefined },
-		brothers: ['🐢', '🐶'],
-	},
+  species: 'tetro',
+  origin: 'Amazon',
+  color: 'silver',
+  size: '1"',
+  family: {
+    dad: '🐠',
+    sister: '🍣',
+    mom: { cool: true, special: undefined },
+    brothers: ['🐢', '🐶'],
+  },
 }
 
 /**
@@ -22,39 +24,42 @@ const theFish = {
 
 // Add a fish
 export async function addAFish() {
-	const fishAdded = await fishDb.insertOne(theFish)
-	console.log('fishAdded ->', fishAdded)
+  const fishAdded = await fishDb.insertOne(theFish)
+														.catch(error => console.error(error))
+  console.log('fishAdded ->', fishAdded)
 }
 
 // Get all fishies
 export async function getAllFish() {
-	const fishList = await fishDb.find({}).toArray()
-	console.table(fishList)
+  const fishList = await fishDb.find({}).toArray()
+								.catch(error => console.error(error))
+  console.table(fishList)
 }
 
 // delete tretro fish ??
 export async function deleteOneFish() {
-	// 1. find the fish we want to delete ???
-	// 2. delete it!
+  // 1. find the fish we want to delete ???
+  // 2. delete it!
+  // const fishDeleted = await fishDb.findOneAndDelete({species: 'tetro'})
+  // find one and delete by id???  WTF?
+  const myCleanId = new ObjectId('65283a1bbcf362bf4cb05574')
 
-	// const fishDeleted = await fishDb.findOneAndDelete({species: 'tetro'})
-	// find one and delete by id???  WTF?
-	const myCleanId = new ObjectId('65283a1bbcf362bf4cb05574')
+  const fishDeleted = await fishDb.findOneAndDelete({ _id: myCleanId })
+																	.catch(error => console.error(error))
 
-	const fishDeleted = await fishDb.findOneAndDelete({ _id: myCleanId })
-	console.log('fishDeleted ->', fishDeleted)
+  console.log('fishDeleted ->', fishDeleted)
 }
 
 // Update Fish
 export async function updateOneFish() {
-	// 1. define the fish we want to update
-	// 2. determine what property want to update
-	// 3. Find the item we want update
-	// 4. Then we update with new data
+  // 1. define the fish we want to update
+  // 2. determine what property want to update
+  // 3. Find the item we want update
+  // 4. Then we update with new data
 
-	const cleanId = new ObjectId('65281c76f90d505d19147e7a')
-	const dataToUpdate = { color: 'red' }
+  const cleanId = new ObjectId('65281c76f90d505d19147e7a')
+  const dataToUpdate = { color: 'red' }
 
-	const updatedFish = await fishDb.findOneAndUpdate({ _id: cleanId }, { $set: dataToUpdate })
-	console.log('updatedFish ->', updatedFish)
+  const updatedFish = await fishDb.findOneAndUpdate({ _id: cleanId }, { $set: dataToUpdate })
+  console.log('updatedFish ->', updatedFish)
 }
